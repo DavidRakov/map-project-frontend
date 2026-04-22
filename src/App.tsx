8ab7dc5a846ac679877ba928cb833b37a49a15c3
DavidRakov <141727866+DavidRakov@users.noreply.github.com>
2026-04-22 09:14:35 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { useEffect } from "react";
-import { Provider } from "react-redux";
-import { store } from "./store";
 import { useAppDispatch } from "./store/hooks";
 import { fetchPolygons } from "./store/thunks/polygonThunks";
 import { fetchObjects } from "./store/thunks/objectThunks";
 import AppLayout from "./components/Layout/AppLayout";
 
-const AppInit = () => {
+const App = () => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -15,14 +13,6 @@ const AppInit = () => {
   }, [dispatch]);
 
   return <AppLayout />;
-};
-
-const App = () => {
-  return (
-    <Provider store={store}>
-      <AppInit />
-    </Provider>
-  );
 };
 
 export default App;
